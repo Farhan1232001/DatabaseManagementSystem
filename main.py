b37ee3677c67ddc7a6ebf44ccfa4893aa9c098b4
Farhan Hussain <farhan.hussain1232001@gmail.com>
2023-05-12 22:07:40 -0700

    Added app icon

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from mainwindow import MainWindow
 from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtGui import QIcon
 from AuthenticationWindow import AuthenticationWindow
+import os
+
+# Set paths to Assets
+current_dir = os.path.dirname(os.path.abspath(__file__))
+appIcon = os.path.join(current_dir, 'assets', 'AppIcon.png')
 
 if __name__ == "__main__":
     
@@ -9,6 +14,7 @@ if __name__ == "__main__":
 
     # Set the light mode style
     app.setStyle('Fusion')
+    app.setWindowIcon(QIcon(appIcon))
 
     # Create all widgets that'll be used in app
     authentication_window = AuthenticationWindow()
