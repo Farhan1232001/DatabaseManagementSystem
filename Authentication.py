463119d92d5a2ba23ca75852acbc382fc04bdef2
Farhan Hussain <farhan.hussain1232001@gmail.com>
2023-05-13 23:05:40 -0700

    finished Setting Ui. Just need to have admin table display

--- Authentication.py
+++ Authentication.py
@@ -1,42 +1,4 @@
-"""
 
-#file name (authentication)
-
-#takes username and password as paramaters
-#returns true if valid 
-#returns false if invalid
-def check_usr(username,password)
-    does username exist 
-        if false return False
-        else 
-            is password correct
-                if false return False
-                else return true
-
-
-#takes username password, and repassword as paramaters
-#returns true if succesfully added 
-#returns false if couldnt add (usr already exist or re password doesnt match)
-def add_usr(username,password,repassword)
-    does username already exist 
-        if true return False
-        else 
-            does password == repassword
-                if false return False
-                else store new username and return True
-            
-
-#takes username (can only use function when signed in)
-#returns true if deleted 
-#returns false if error (username doesnt exist or only one usr remaining )
-def delete_usr(username)
-    is there atleast 2 usernames
-        if true
-            does username already exist 
-                if true delete user &return true
-                else return False
-        else return false
-            """
 
 import sqlite3
 import hashlib
@@ -45,26 +7,27 @@ class Authentication:
     def __init__(self, db_file='students.db'):
         self.con = sqlite3.connect(db_file)
         self.cur = self.con.cursor()
+        
 
         # Create the authentication table if it doesn't exist
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS authentication (
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS Administrators (
                             username TEXT PRIMARY KEY,
                             password TEXT NOT NULL
                         )''')
 
         # Add a default user and password if the database is being created for the first time
-        self.cur.execute('SELECT COUNT(*) FROM authentication')
+        self.cur.execute('SELECT COUNT(*) FROM Administrators')
         count = self.cur.fetchone()[0]
         if count == 0:
             hashed_password = hashlib.sha256('password'.encode('utf-8')).hexdigest()
-            self.cur.execute('INSERT INTO authentication VALUES (?, ?)', ('admin', hashed_password))
+            self.cur.execute('INSERT INTO Administrators VALUES (?, ?)', ('admin', hashed_password))
             self.con.commit()
 
     # Check if a user is valid
     def check_usr(self, username, password):
-        """Returns true if valid"""
+        """Checks if usrname/passwrd in admin tableReturns true if valid"""
         # Execute a SELECT query to check if the username exists
-        self.cur.execute('SELECT * FROM authentication WHERE username=?', (username,))
+        self.cur.execute('SELECT * FROM Administrators WHERE username=?', (username,))
         row = self.cur.fetchone()
         if row is None:
             return False
@@ -77,10 +40,50 @@ class Authentication:
             else:
                 return False
 
+
+    def check_username(self, username):
+        """Checks if usrname in admin tableReturns true if it exists"""
+        # Execute a SELECT query to check if the password exists
+        self.cur.execute('SELECT * FROM Administrators WHERE username=?', (username,))
+        row = self.cur.fetchone()
+        if row is None:
+            return False
+        else:
+            return True
+
+    def check_password(self, password):
+        """Checks if the given password exists in the admin table.
+        
+        Returns:
+        bool: True if the password exists, False otherwise.
+        """
+        # Execute a SELECT query to check if the password exists
+        self.cur.execute('SELECT * FROM Administrators WHERE password=?', (password,))
+        row = self.cur.fetchone()
+
+        if row is None:
+            return False
+        else:
+            # Hash the input password using the SHA-256 algorithm
+            hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            # Check if the hashed password matches the one in the database
+            if row[1] == hashed_password:
+                return True
+            else:
+                return False
+
+
+    def getAdminTable(self):
+        """Returns the contents of the Administrators table"""
+        self.cur.execute('SELECT * FROM Administrators')
+        rows = self.cur.fetchall()
+        return rows
+
+
     # Add a new user to the database
     def add_usr(self, username, password, repassword):
         # Execute a SELECT query to check if the username already exists
-        self.cur.execute('SELECT * FROM authentication WHERE username=?', (username,))
+        self.cur.execute('SELECT * FROM Administrators WHERE username=?', (username,))
         row = self.cur.fetchone()
         if row is not None:
             return False
@@ -92,19 +95,34 @@ class Authentication:
                 # Hash the password using the SHA-256 algorithm
                 hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
                 # Execute an INSERT query to add the new user to the database
-                self.cur.execute('INSERT INTO authentication VALUES (?, ?)', (username, hashed_password))
+                self.cur.execute('INSERT INTO Administrators VALUES (?, ?)', (username, hashed_password))
                 self.con.commit()
                 return True
 
     # Delete a user from the database
     def delete_usr(self, username):
         # Execute a SELECT query to check if there are at least two usernames in the database
-        self.cur.execute('SELECT COUNT(*) FROM authentication')
+        self.cur.execute('SELECT COUNT(*) FROM Administrators')
         count = self.cur.fetchone()[0]
         if count < 2:
             return False
         else:
             # Execute a DELETE query to delete the user from the database
-            self.cur.execute('DELETE FROM authentication WHERE username=?', (username,))
+            self.cur.execute('DELETE FROM Administrators WHERE username=?', (username,))
             self.con.commit()
             return True
+        
+    def isPasswordValid(self, password):
+        """Checks if the given password can be used to create a new admin"""
+        # Hash the input password using the SHA-256 algorithm
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        # Execute a SELECT query to check if the hashed password exists
+        self.cur.execute('SELECT * FROM Administrators WHERE password=?', (hashed_password,))
+        row = self.cur.fetchone()
+
+        if row is None:
+            return True
+        else:
+            return False
+
--- SettingsDialog.py
+++ SettingsDialog.py
@@ -1,31 +1,99 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QMessageBox, QTableWidgetItem
 from Ui_SettingsWindow import Ui_SettingsWindow
 from Authentication import Authentication
+
 
 class SettingsDialog(QDialog):
     def __init__(self, parent=None):
         super(SettingsDialog, self).__init__(parent)
-        self.setup_ui()
-
-        self.parent = parent
-
-        authenticationManager = Authentication()
-
-    def setup_ui(self):
-        # Create instance of the generated UI class
         self.ui = Ui_SettingsWindow()
         self.ui.setupUi(self)
 
+        self.parent = parent
+        self.tableWidget = self.ui.adminInfo_tableWidget
+
+        self.authenticationManager = Authentication()
+
+        self.setup_ui()
+
+    def setup_ui(self):
+       
+
+        # Connect signals and slots
+        # ----------------------------------------------------------------
+        self.ui.add_admin_btn.clicked.connect(self.addAdministrator)
+
         # Set attributes of the dialog
         self.setWindowTitle("Settings")
+        self.tableWidget.setItem(0,0,QTableWidgetItem("10"))
+
+        #self.fillInAdminTable()
+
+    def fillInAdminTable(self):
+        table = self.authenticationManager.getAdminTable()
+
+        # Set the number of rows and columns in the table widget
+        num_rows = len(table)
+        num_cols = len(table[0])
+        self.tableWidget.setRowCount(num_rows)
+        self.tableWidget.setColumnCount(num_cols)
+
+        # Iterate over the table data and insert it into the table widget
+        for row in range(num_rows):
+            for col in range(num_cols):
+                item = QTableWidgetItem(str(table[row][col]))
+                self.tableWidget.setItem(row, col, item)
+
 
     def addAdministrator(self):
         # Check inputs
-        username = self.ui.username_lineEdit
-        password = self.ui.password_lineEdit
+        username = self.ui.username_lineEdit.text()
+        password = self.ui.password_lineEdit.text()
+
+        if username == "" or password == "": return
+
+        if not self.authenticationManager.check_password(password) and not self.authenticationManager.check_username(username):
+            if self.authenticationManager.isPasswordValid(password):
+                if self.authenticationManager.add_usr(username, password, password):
+                    QMessageBox.information(self, "Admin Added", "Administrator added successfully.")
+                    print("Admin added")
+                else:
+                    QMessageBox.warning(self, "Error", "Failed to add administrator. Username or password already in use.")
+                    print("Username/password already in use")
+            else:
+                QMessageBox.warning(self, "Error", "Failed to add administrator. Username or password already in use.")
+                print("Password already in use")
+        else:
+            QMessageBox.warning(self, "Error", "Failed to add administrator. Username and password already in use.")
+            print("Username/password already in use")
+
+        self.ui.username_lineEdit.clear()
+        self.ui.password_lineEdit.clear()
+
+    def deleteAdministrator(self):
+        # Check inputs
+        username = self.ui.username_lineEdit.text()
+        password = self.ui.password_lineEdit.text()
+
+        if username == "" or password == "": return
 
         if self.authenticationManager.check_usr(username, password):
-            pass
+            self.authenticationManager.delete_usr(username)
+
+        self.ui.username_del_lineEdit.clear()
+        self.ui.password_del_lineEdit.clear()
+
+
+
+        # if self.authenticationManager.check_usr(username, password):
+        #     print("Admin cannot be added b/c username&password taken")
+        # else:
+        #     # add admin
+        #     if not self.authenticationManager.check_password(password):
+                
+        #     else:
+        #         print("Password already in use")
+            
 
 
 
